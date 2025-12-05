0a4174771c709f28cc86d749cf587b5d120cc0d6
Xavier Jin <xavierbunbunny@gmail.com>
2025-12-04 22:53:45 -0800

    Day 4 complete!

---
+++ D4/D4P2.py
@@ -0,0 +1,25 @@
+data = open("input.txt", "r").read().split("\n")
+
+def clamp(value, minimum, maximum):
+    return minimum if value < minimum else maximum if value > maximum else value
+
+for i in range(len(data)):
+    data[i] = list(data[i])
+
+count = 0
+removed = True
+while removed:
+    removed = False
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j] == "@":
+                area = data[clamp(i - 1, 0, len(data)) : clamp(i + 1, 0, len(data)) + 1]
+                components = []
+                for k in range(len(area)):
+                    components += area[k][clamp(j - 1, 0, len(data[i])) : clamp(j + 1, 0, len(data[i])) + 1]
+                if components.count("@") < 5:
+                    count += 1
+                    removed = True
+                    data[i][j] = "X"
+    print(data)
+print(count)
