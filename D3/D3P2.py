cd68e2016539ae97fbdd33273c9ee6eccd913070
Xavier Jin <xavierbunbunny@gmail.com>
2025-12-04 21:41:45 -0800

    Day 3 Part 2 done!

--- D3/D3P2.py
+++ D3/D3P2.py
@@ -2,11 +2,18 @@ data = open("input.txt", "r").read().split("\n")
 
 summation = 0
 for bank in data:
+    number = ""
     bank = list(bank)
     for i in range(len(bank)):
         bank[i] = int(bank[i])
-    
-    
-    
-print(summation)
+   
+    maximum = max(bank[:len(bank) - 11])
+    number += str(maximum)
+    bank = bank[bank.index(maximum) + 1:]
+    for i in range(10, -1, -1):
+        maximum = max(bank[:len(bank) - i])
+        number += str(maximum)
+        bank = bank[bank.index(maximum) + 1:]
+    summation += int(number)
 
+print(summation)
